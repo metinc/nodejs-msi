a4b002d184f6a0be3e9918a06e3a40ed0ec8f8b8
Metin Celik <Celik_Metin@cat.com>
2019-04-08 16:03:45 +0200

    Use process.env.PORT

--- server.js
+++ server.js
@@ -1,6 +1,29 @@
-var http = require('http');
+const http = require('http');
+
+const port = normalizePort(process.env.PORT || '9000');
 
 http.createServer(function (req, res) {
   res.write('hello world');
   res.end();
-}).listen(9000);
+}).listen(port);
+
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  let port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
