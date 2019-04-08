8ad13a640c1e5f6eb4c7084b0d16e237d15b79d5
Metin Celik <Celik_Metin@cat.com>
2019-04-08 14:29:25 +0200

    Add localization file for en-us

--- installer/build.ps1
+++ installer/build.ps1
@@ -19,7 +19,7 @@ $wix_dir="c:\Program Files (x86)\WiX Toolset v3.11\bin"
 
 . "$wix_dir\heat.exe" dir $source_dir -srd -dr INSTALLDIR -cg MainComponentGroup -out directory.wxs -ke -sfrag -gg -var var.SourceDir -sreg -scom -t exclude_files.xslt
 . "$wix_dir\candle.exe" -dSourceDir="$source_dir" *.wxs -o output\ -ext WiXUtilExtension
-. "$wix_dir\light.exe" -o output\installer.msi output\*.wixobj -cultures:en-US -ext WixUIExtension.dll -ext WiXUtilExtension
+. "$wix_dir\light.exe" -o output\installer.msi output\*.wixobj -cultures:en-us -loc en-us.wxl -ext WixUIExtension.dll -ext WiXUtilExtension
 
 # optional digital sign the certificate. 
 # you have to previously import it.
